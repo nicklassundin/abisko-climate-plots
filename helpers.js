f33194efeae207389bb8ffa909fd8f91f4dff209
Anton Eriksson <aner0164@student.umu.se>
2018-06-26 19:41:06 +0200

    More work

--- helpers.js
+++ helpers.js
@@ -1,47 +1,42 @@
-var months = function () {
-    return ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-};
+var months = () => ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-var sum = function (values) {
-    return values.reduce(function (sum, value) {
-        return sum + value;
-    }, 0);
-}
+var monthByIndex = index => months()[index];
 
-var average = function (values) {
+var monthName = month => ({
+    jan: 'January', feb: 'February', mar: 'March', apr: 'April', may: 'May', jun: 'June',
+    jul: 'July', aug: 'August', sep: 'September', oct: 'October', nov: 'November', dec: 'December'
+})[month];
+
+var summerMonths = ['may', 'jun', 'jul', 'aug', 'sep'];
+var winterMonths = ['oct', 'nov', 'dec', 'jan', 'feb', 'mar', 'apr'];
+
+var isSummerMonth = month => summerMonths.includes(month);
+var isWinterMonth = month => winterMonths.includes(month);
+
+var sum = values => values.reduce((sum, current) => sum + current, 0);
+
+var min = values => values.reduce((min, current) => Math.min(min, current), Infinity);
+
+var max = values => values.reduce((max, current) => Math.max(max, current), -Infinity);
+
+var average = (values) => {
     if (values.length === 0) return 0;
     return sum(values) / values.length;
 };
 
-var movingAverage = function (values, index, number) {
-    var startIndex = Math.max(index - number, 0);
-    return average(values.slice(startIndex, index));
-};
+var mean = average;
 
-var movingAverages = function (values, number) {
-    return values.map(function (_, index) {
-        return movingAverage(values, index, number);
-    });
-};
+var movingAverage = (values, index, number) => average(values.slice(Math.max(index - number, 0), index));
 
-var difference = function (values, baseline) {
-    return values.map(function (value) {
-        return value - baseline;
-    });
-};
+var movingAverages = (values, number) => values.map((_, index) => movingAverage(values, index, number));
 
-var sumSquareDistance = function (values, mean) {
-    return values.reduce(function (sum, value) {
-        return sum + Math.pow(value - mean, 2);
-    }, 0);
-};
+var difference = (values, baseline) => values.map(value => value - baseline);
 
-var variance = function (values) {
-    var mean = average(values);
-    return sumSquareDistance(values, mean) / (values.length - 1)
-};
+var sumSquareDistance = (values, mean) => values.reduce((sum, value) => sum + Math.pow(value - mean, 2), 0);
 
-var confidenceInterval = function (mean, variance, count) {
+var variance = values => sumSquareDistance(values, average(values)) / (values.length - 1);
+
+var confidenceInterval = (mean, variance, count) => {
     var zs = [0, 12.706, 4.303, 3.182, 2.776, 2.571, 2.447, 2.365, 2.306, 2.262, 2.228, 2.201, 2.179];
     var ci = zs[count - 1] * Math.sqrt(variance / count);
     return {
@@ -53,16 +48,14 @@ var confidenceInterval = function (mean, variance, count) {
 var baselineLower = 1961;
 var baselineUpper = 1990;
 
-var isBaselinePeriod = function (year) {
-    return year >= baselineLower && year <= baselineUpper;
-};
+var withinBaselinePeriod = year => year >= baselineLower && year <= baselineUpper;
 
-var validNumber = function (string) {
+var validNumber = (string) => {
     var number = +string;
     return (number) ? number : undefined;
 };
 
-var parseDate = function (string) {
+var parseDate = (string) => {
     var date = string.split('-');
     return {
         year: +date[0],
@@ -71,35 +64,14 @@ var parseDate = function (string) {
     };
 };
 
-var parseNumber = function (string) {
-    return parseFloat(string.replace(',', '.')) || undefined;
-};
+var parseNumber = string => parseFloat(string.replace(',', '.')) || undefined;
 
-var combine = function (xs, ys) {
-    return xs.map(function (x, index) {
-        return {
-            x: x,
-            y: ys[index],
-        };
-    });
-};
-
-var linearRegression = function (xs, ys) {
-    var data = xs.map(function (x, index) {
-        return [x, ys[index]];
-    });
-
+var linearRegression = (xs, ys) => {
+    var data = xs.map((x, index) => [x, ys[index]]);
     var result = regression.linear(data);
     var gradient = result.equation[0];
     var intercept = result.equation[1];
-
-    var linear = function (x) {
-        return gradient * x + intercept;
-    };
-
-    linear.toString = function() {
-        return gradient + ' x ' + (intercept >= 0 ? '+' : '-') + Math.abs(intercept);
-    };
-
+    var linear = x => gradient * x + intercept;
+    linear.toString = () => gradient + ' x ' + (intercept >= 0 ? '+' : '-') + Math.abs(intercept);
     return linear;
 };
