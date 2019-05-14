d57f2b7c27914d52dfd8d594b4999c662dbc041e
nicklassundin <p4cr@hotmail.com>
2019-05-14 18:37:10 +0200

    Create abisko-climate-plots-lib.js

---
+++ abisko-climate-plots-lib.js
@@ -0,0 +1,18 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://code.highcharts.com/highcharts.js"></script>
+		<script src="https://code.highcharts.com/modules/annotations.js"></script>
+		<script src="https://code.highcharts.com/highcharts-more.js"></script>
+		<script src="https://code.highcharts.com/modules/series-label.js"></script>
+		<script src="https://code.highcharts.com/modules/exporting.js"></script>
+		<script src="https://code.highcharts.com/modules/export-data.js"></script>
+		<script src="https://rawgit.com/phpepe/highcharts-regression/master/highcharts-regression.js"></script>
+
+<script src="loader.js"></script>
+<script src="f_stats.js"></script>
+<script src="helpers.js"></script>
+<script src="init.js"></script>
+<script src="papaparse.min.js"></script>
+
+<script src="regression.js"></script>
+<script src="render.js"></script>
+
