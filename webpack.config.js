3fb01c44b0572b79cef1a430a23110043bb08a60
nicklassundin <ada08njo@student.lu.se>
2020-01-24 13:26:03 +0100

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+    		bundle: './modules/lib.js'
+  	},
+	output: {
+		filename: './[bundle].js',
+    		path: __dirname + '/client'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.txt$/, 
+				loader: 'raw-loader'
+			},{
+				test: /\.csv$/,
+				loader: 'csv-loader',
+				// options: {
+					// header: true,
+					// download: true,
+					// skipEmptyLines: true
+				// }
+			}
+		]
+	}
+};
+
