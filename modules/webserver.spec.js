1437d72076abac7a0fba6b4c2f2da79083760bee
nicklassundin <ada08njo@student.lu.se>
2020-01-30 15:34:55 +0100

    generelize function for drawing temp and precipitation charts && introduced some templates ids

---
+++ modules/webserver.spec.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const request = require('request')
+
+const expect = require('chai').expect
+const express = require('express');
+
+const webserver = require('./webserver.js')
+
+describe('javascript statistic module', function () {
+		
+	it('HTTP - start', function * () {
+		var app = express();
+		var result = webserver.http(app).then(function(){Done()});
+		//TODO 
+	})
+	it('HTTPS - start', function * () {
+		var app = express();
+		var result = webserver.https(app).then(function(){Done()}); 
+		//TODO 
+	})
+})
