52aa00df1ce8530426047d1bd75d349a101207e1
nicklassundin <ada08njo@student.lu.se>
2020-06-22 10:06:11 +0200

    iceThickDate tooltip date

--- modules/config/tooltips.js
+++ modules/config/tooltips.js
@@ -14,6 +14,22 @@ exports.formatters = {
 		})
 		return tooltip;
 	},
+	winterValueDate: function(){
+		var tooltip = '<span style="font-size: 10px">Winter ' + (this.x + '-' + (this.x+1)) + '</span><br/>';
+		this.points.forEach(point =>{
+
+			tooltip += '<span style="font-size: 10px">'+point.point.date+'</span><br/>'
+			tooltip += '<span style="color:'+
+				point.color +
+				'">\u25CF</span> ' +
+				point.series.name +
+				': <b>'+
+				(point.y) + 
+				'</b><br/>'
+		});
+		return tooltip;
+
+	},
 	default: undefined,
 }
 
