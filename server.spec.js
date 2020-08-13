e4f26600db36e74deb3e7fbf97f714169892d21e
nicklassundin <ada08njo@student.lu.se>
2020-08-13 14:18:07 +0200

    some adjustment

--- server.spec.js
+++ server.spec.js
@@ -6,24 +6,24 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('server test', function() {
-	// before(function(){
-	// })
-	// it('should list ALL / GET', function(done) {
-	// 	chai.request(server)
-	// 		.get('/')
-	// 		.end(function(err, res){
-	// 			res.should.have.status(200);
-	// 			done();
-	// 		});
-	// });
-	// it('should list ALL /chart GET', function(done) {
-	// 	chai.request(server)
-	// 		.get('/chart')
-	// 		.end(function(err, res){
-	// 			res.should.have.status(200);
-	// 			done();
-	// 		});
-	// });
+	before(function(){
+	})
+	it('should list ALL / GET', function(done) {
+		chai.request(server)
+			.get('/')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			});
+	});
+	it('should list ALL /chart GET', function(done) {
+		chai.request(server)
+			.get('/chart')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			});
+	});
 	// it('chart time to load', function * (){
 	// chai.request(server)
 	// .get('/chart')
