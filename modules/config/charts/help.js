0b04609b1304c8c7d42e9ce5f04cd4cc04d76da6
nicklassundin <ada08njo@student.lu.se>
2021-04-23 15:02:20 +0200

    regenizzed

---
+++ modules/config/charts/help.js
@@ -0,0 +1,28 @@
+
+const preset = require('./parse.config.json');
+var before = {
+	smhi:  function(result){ 
+		result = result.split('\n') 
+		var line = result.findIndex(x => x.indexOf('Tidsutsnitt:') > -1) 
+		var rest = result.splice(0,line);
+		result = result.join('\r\n'); 
+		rest = rest[1].split(';');
+		global.stationName = rest[0];
+		result.name = rest[0];
+		return result 
+	},
+	abisko: function(result){ 
+		result.name = 'Abisko';
+		global.stationName = 'Abisko'; 
+	}
+}
+exports.preset = function(id){
+	var res = preset[id];
+	if(res.preset.beforeFirstChunk){
+		var be = res.preset.beforeFirstChunk;
+		res.preset.beforeFirstChunk = before[be];
+	}
+	return res;
+}
+
+
