e71a95562749c3f14020e94af2f70bb980226afa
nicklassundin <ada08njo@student.lu.se>
2021-04-23 09:30:15 +0200

    fix monthlyTemperature slip

--- app.js
+++ app.js
@@ -76,55 +76,7 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		database.admin.then(function(connection){
-			connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-				if (results.length > 0) {
-					request.session.loggedin = true;
-					request.session.username = username;
-					response.render('upload.pug', 
-						{
-							username: username,
-							password: password
-						});
-				} else {
-					response.send('Incorrect Username and/or Password!');
-				}			
-				response.end();
-			});
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
 
-
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
-});
-
-app.post('/admin/create/table', function(request, response, next){
-	if(request.session.loggedin){
-		console.log(request)
-		var username = request.body.username;
-		var password = request.body.password;
-		var tablename = request.body.tablename;
-		var columns = request.body.columns;
-		database.createTable(tablename, columns, database.admin);
-		response.send('Created new table '+tablename)
-	} else {
-		response.send('Please login to view this page!')
-	}
-})
 
 const url = require('url');
 const custom = require('./config/preset.js').preset;
