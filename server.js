b3638e8cc49332a5e49267de05d331b36724a2c1
nicklassundin <ada08njo@student.lu.se>
2021-04-29 10:59:02 +0200

    remove https

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ if(process.argv.includes('d')){
 	var app = require('./dev-app.js').app
 	var web = require('./modules/server/web.js')
 	web.webserver["http"](app);
-	web.webserver["https"](app);
 }else{
 	var app = require('./app.js').app
 	var web = require('./modules/server/web.js')
 	web.webserver["http"](app);
-	web.webserver["https"](app);
 }
 
 
