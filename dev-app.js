3fdb4fbfd6632b3f5b7b82149467f7d80f7a5155
nicklassundin <ada08njo@student.lu.se>
2021-05-03 16:24:07 +0200

    fix preview

--- dev-app.js
+++ dev-app.js
@@ -8,8 +8,88 @@ try{
 }
 var fs = require('fs');
 var express = require('express');
-var lib = require('./app.js');
-var app = lib.app;
+
+// Pre-setup
+var $ = require("jquery");
+
+
+// Setup
+const app = express();
+var hbs = require('hbs');
+// Open file access
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/dep', express.static(__dirname + '/dep'));
+app.use('/modules', express.static(__dirname + '/modules'));
+app.use('/config', express.static(__dirname + '/config'));
+app.use('/data', express.static(__dirname + '/data'));
+app.use('/data/abisko', express.static(__dirname + '/data/abisko'));
+app.use('/client', express.static(__dirname + '/client'));
+app.use('/tmp', express.static(__dirname + '/tmp'));
+app.use('/maps', express.static(__dirname + '/maps'));
+
+
+// SMHI DB connection
+const TYPE = 'corrected-archive';
+require('./modules/server/smhi').init(app, TYPE);
+
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+const url = require('url');
+const custom = require('./config/preset.js').preset;
+exports.custom = custom;
+
+var charts = (req) => {
+	return new Promise((res, rej) => {
+		custom.then(IDs => {
+			const queryObject = url.parse(req.url,true).query;
+			var id;
+			var ids;
+			if(!queryObject.id) {
+				ids = IDs.all;
+			}else if(!custom[queryObject.id]){
+				ids = queryObject.id.split(",");
+			}else{
+				ids = IDs[queryObject.id];
+			}
+			res(ids.map(id => {
+				return {
+					id: id,
+					station: queryObject.station
+				}
+			}))
+		})
+	})
+}
+app.get( '/chart', (req, res) => {
+	charts(req).then(chrts => {
+		res.render('chart.hbs', {
+			chrts
+		})
+	})
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+custom.then(chrts => {
+	stations = ["abisko", "53430", "global"];
+	app.get('/browse', (req, res) => {
+		res.render('browse.hbs', {
+			chrts,
+			stations,
+			latestCommit
+		})
+	})
+})
+
+/////
 
 var fileWrite = function(json, file){
 	fs.exists(file, function (exists) {
@@ -22,7 +102,7 @@ var fileWrite = function(json, file){
 		}
 	})
 }
-lib.custom.then((json) => {
+custom.then((json) => {
 	fileWrite(json, __dirname+'/temp/preset.json') 
 })
 const merger = require('./modules/config/charts/merge.js').preset;
@@ -30,7 +110,7 @@ merger.then((json) => {
 	fileWrite(json, __dirname+'/temp/modules.config.charts.merge.json')
 })
 
-lib.custom.then(chrts => {
+custom.then(chrts => {
 	stations = ["abisko", "53430", "global"];
 	app.get('/static', (req, res) => {
 		stations = ["abisko", "53430", "global"];
