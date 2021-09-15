f17c35e8595cdfa55be81bb653e97186a313f728
nicklassundin <ada08njo@student.lu.se>
2021-09-15 11:20:34 +0200

    update

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Pre-setup
 var $ = require("jquery");
 if(process.argv.includes('d')){
-	var app = require('./dev-app.js').app
-	var web = require('./modules/server/web.js')
-	web.webserver["http"](app);
-	app.get( '/', (req, res) => {
-		res.send('Lets do this');
-	});
-	app.use('/health', require('express-healthcheck')());
+	require('./config/gen.js').genStaticFiles(__dirname).then(() => {
+		var app = require('./dev-app.js').app
+		var web = require('./modules/server/web.js')
+		web.webserver["http"](app);
+		app.get( '/', (req, res) => {
+			res.send('Lets do this');
+		});
+		app.use('/health', require('express-healthcheck')());
+	})
 }else{
 	var app = require('./app.js').app
 	var web = require('./modules/server/web.js')
