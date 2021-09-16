5af015db94394e6a4a7b9cb2eef16ee81fad1345
nicklassundin <ada08njo@student.lu.se>
2021-09-16 11:33:47 +0200

    smaller changes

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-// Pre-setup
-var $ = require("jquery");
-var generate = require('climate-plots-config')
-console.log(generate)
-if(process.argv.includes('d')){
-	generate.genStaticFiles(__dirname).then(() => {
+
+require('climate-plots-config').genStaticFiles(__dirname).then(() => {
+	// Pre-setup
+	var $ = require("jquery");
+	if(process.argv.includes('d')){
 		var app = require('./dev-app.js').app
 		var web = require('./modules/server/web.js')
 		web.webserver["http"](app);
@@ -11,15 +10,15 @@ if(process.argv.includes('d')){
 			res.send('Lets do this');
 		});
 		app.use('/health', require('express-healthcheck')());
-	})
-}else{
-	var app = require('./app.js').app
-	var web = require('./modules/server/web.js')
-	web.webserver["http"](app);
-	app.get( '/', (req, res) => {
-		res.send('Lets do this');
-	});
-	app.use('/health', require('express-healthcheck')());
-}
+	}else{
+		var app = require('./app.js').app
+		var web = require('./modules/server/web.js')
+		web.webserver["http"](app);
+		app.get( '/', (req, res) => {
+			res.send('Lets do this');
+		});
+		app.use('/health', require('express-healthcheck')());
+	}
+})
 
 
