0fc202bb06b648d9dc5676f4717755330324c4b1
nicklassundin <ada08njo@student.lu.se>
2021-09-23 09:26:01 +0200

    remove dubblicated code

--- server.js
+++ server.js
@@ -2,26 +2,17 @@
 require('climate-plots-config').genStaticFiles(__dirname).then(() => {
 	// Pre-setup
 	var $ = require("jquery");
+	var app = require('./app.js').app
+	var web = require('./modules/server/web.js')
 	if(process.argv.includes('d')){
-		// var app = require('./dev-app.js').app
-		var app = require('./app.js').app
 		var dev = require('./dev.js')
 		dev.setup(app)
-		var web = require('./modules/server/web.js')
-		web.webserver["http"](app);
-		app.get( '/', (req, res) => {
-			res.send('Lets do this');
-		});
-		app.use('/health', require('express-healthcheck')());
-	}else{
-		var app = require('./app.js').app
-		var web = require('./modules/server/web.js')
-		web.webserver["http"](app);
-		app.get( '/', (req, res) => {
-			res.send('Lets do this');
-		});
-		app.use('/health', require('express-healthcheck')());
 	}
+	web.webserver["http"](app);
+	app.get( '/', (req, res) => {
+		res.send('Lets do this');
+	});
+	app.use('/health', require('express-healthcheck')());
 })
 
 
