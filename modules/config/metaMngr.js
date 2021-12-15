7e8a15c9c314ff335c19a97d90faca28644518be
nicklassundin <p4cr@hotmail.com>
2021-12-15 09:31:29 +0100

    fix language replacement from replace to replaceAll

--- modules/config/metaMngr.js
+++ modules/config/metaMngr.js
@@ -158,7 +158,7 @@ exports.meta = {
             try {
 
                 // TODO order of month replace for subsets
-                var res = res.replace(
+                var res = res.replaceAll(
                         "[stationName]",
                         meta.stationDef.stationName
                     ),
@@ -176,37 +176,37 @@ exports.meta = {
                 res = (meta.subset
                     ? meta.subset.enabled
                     : false)
-                    ? res.replace(
+                    ? res.replaceAll(
                         "[month]",
                         set
                     )
-                    : res.replace(
+                    : res.replaceAll(
                         "[month]",
                         meta.month
                     );
-                res = res.replace(
+                res = res.replaceAll(
                     "[baselineLower]",
                     baselineLower
                 );
-                res = res.replace(
+                res = res.replaceAll(
                     "[baselineUpper]",
                     baselineUpper
                 );
-                res = res.replace(
+                res = res.replaceAll(
                     "[baseline]",
                     `${baselineLower} - ${baselineUpper}`
                 );
-                res = res.replace(
+                res = res.replaceAll(
                     "[CO2]",
                     `CO${"2".sub()}`
                 );
-                res = res.replace(
+                res = res.replaceAll(
                     "[SOME TEXT]",
                     ""
                 );
                 if (meta.extreme) {
 
-                    res = res.replace(
+                    res = res.replaceAll(
                         "[lim]",
                         (meta.extreme.lim > 0
                             ? "+"
@@ -222,14 +222,14 @@ exports.meta = {
 
                 }
                 if (meta.unitType && meta.units && meta.units[meta.unitType] != undefined) {
-                    var res = res.replace(
+                    var res = res.replaceAll(
                         "[unit]",
                         meta.units[meta.unitType].singular
-                    ).replace(
+                    ).replaceAll(
                         "[units]",
                         meta.units[meta.unitType].plural
                     ).
-                        replace(
+                        replaceAll(
                             "[interval]",
                             meta.units[meta.unitType].interval
                         );
