f33a0881c12bffabf2e59f417e151a47ffdef803
nicklassundin <p4cr@hotmail.com>
2024-11-20 12:26:13 +0100

    fix(bash): bashfile

---
+++ setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create a virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install required Python packages
+pip install -r requirements.txt
