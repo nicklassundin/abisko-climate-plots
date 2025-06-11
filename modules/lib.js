aef6193eecb986cb284d893c729ff0e9c5107328
nicklassundin <p4cr@hotmail.com>
2025-06-11 11:07:37 +0200

    save

--- modules/lib.js
+++ modules/lib.js
@@ -1,4 +1,5 @@
 //exports.module = require('../../renderer/lib');
-lib = require('vizchange-plot-builder');
+// lib = require('vizchange-plot-builder');
+lib = require('./plot-render');
 //if(process.env.NODE_ENV === 'development') lib = require('../../renderer/lib');
 exports.module = lib;
