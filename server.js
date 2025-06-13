31c14b59a41cc8ca5bde9f2733eea8f875b157a2
nicklassundin <p4cr@hotmail.com>
2025-06-13 13:50:13 +0200

    save remove cache and un needed

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const health = require('express-healthcheck'); // Health Check
 require('jquery');
 const { parse  } = require('csv-parse/sync');
 // Cache requirements
-const fs = require('fs');   // write files to cache etc TODO make obsolete
+const fs = require('fs'); 
 const axios = require('axios');
-const { setupCache } = require('axios-cache-interceptor');
-const { buildStorage } = require('axios-cache-interceptor');
 // precalculated
-let stats = require('./modules/plot-render/modules/stats/module.js');
-const stats_configs = JSON.parse(JSON.stringify(stats.configs['production_redirect']));
 // general express requirements
 require('request');
 const express = require('express');
@@ -25,47 +21,7 @@ const path = require('path');
 const plots_config = require('climate-plots-config');
 const web = require('./modules/server/web.js');
 const STATIC_STATIONS = require('./static/charts/stations.json');
-// Getting smhi server list
-let smhi = require('vizchange-smhi');
 const config = require("./static/server.config.json");
-
-const storage = buildStorage({
-    find(key) {
-        return new Promise((resolve, reject) => {
-            if(!fs.existsSync(`./cache/storage/${key}.json`)){
-                resolve(undefined)
-            }else{
-                fs.readFile(`./cache/storage/${key}.json`, 'utf8', function(error, data) {
-                    if(error) reject(error);
-                    if(JSON.parse(data).length <= 0){
-                        resolve(undefined)
-                    }else{
-                        resolve(JSON.parse(data))
-                    }
-                })
-            }
-        })
-    },
-    set(key, value, req) {
-        try{
-            fs.writeFileSync(`./cache/storage/${key}.json`, JSON.stringify(value))
-        }catch(error){
-            throw error;
-        }
-    },
-    remove(key) {
-        //fs.unlink(`./cache/storage/${key}.json`);
-    }
-})
-/**
- * Axios Cache
- */
-setupCache(axios, {
-    cache: {
-        ttl: 1000*60*60*24*365
-    },
-    storage    // TODO make storage work
-})
 
 /**
  * Class representing Server instance
@@ -106,9 +62,8 @@ class Server {
         // Setup Browse preview on server
         this.setupServerPreview();
         // setup Cache
-        this.setupCache();
         // setup pre-calculated values TODO make obsolete
-        this.precalculation();
+        // this.precalculation();
 
         this.app.use(cors({
             origin: ['*']
@@ -151,10 +106,6 @@ class Server {
         return STATIC_STATIONS;
     }
 
-    /**
-     * get list of station from both pre-set list and smhi API list
-     * @returns {Promise}
-     */
     get stationList() {
 	    return Promise.resolve().then(() => {
 	    return {
@@ -166,93 +117,6 @@ class Server {
 	    	})
 	    }
 	    })
-        // this.smhiAPI();
-        // return this.smhi_stations().then((smhiStations) => {
-        //     return {
-        //         smhi: smhiStations,
-        //         fixed: Object.keys(this.STATIC_STATIONS).map(value => {
-        //             return {
-        //                 id: value
-        //             }
-        //         })
-        //     }
-        // })
-    }
-    /**
-     * Setting up cache data api for request against server
-     */
-    setupCache(){
-        // setup cache
-        const config = require('./static/server.config.json');
-        this.app.use('/data/:server/:params', async function(req, res) {
-            let url = `https://${config[req.params.server]}${req['_parsedUrl'].search}`
-            const { data } = await axios.get(url)
-            res.setHeader('Content-Type', 'application/json')
-            res.send(data)
-        })
-    }
-    /**
-     * get Precalculated data or generate it if not in registry
-     */
-    precalculation() {
-        this.app.get('/precalculated/:station/:type/*', (req, res) => {
-            let specs = JSON.parse(JSON.stringify(stats_configs))
-            specs.station = req.params.station;
-            specs.type = req.params.type
-            req.params['0'] = req.params['0'].split('/')
-            // TODO recreate req.params so no need for unshift
-            req.params['0'].unshift(req.params.type)
-
-            let search = req.query
-            if(!search) search = {}
-            Object.keys(search).forEach(key => {
-                switch (key) {
-                    case 'baselineStart':
-                        specs.baseline.start = Number(search[key])
-                        break;
-                    case 'baselineEnd':
-                        specs.baseline.end = Number(search[key])
-                        break;
-                    case 'start':
-                    case 'end':
-                        specs.dates[key] = Number(search[key])
-                        break;
-                    default:
-                        specs[key] = search[key];
-                }
-            })
-            /**
-             * file path for precalculated data
-             * @type {string}
-             */
-            let filePath = `./cache/${specs.station}_${specs.type}_${req.params['0'].join('_')}${req['_parsedUrl'].search}.json`
-            if (fs.existsSync(filePath)) {
-                fs.readFile(filePath, 'utf8', function(error, data) {
-                    res.send(data)
-                })
-            }else{
-                console.error('No cache file found')
-                const result = stats.getByParams(specs, req.params['0'])
-                result.then((resolved) => {
-                    if(Array.isArray(resolved) && typeof resolved[0].then === 'function'){
-                        return Promise.all(resolved).then(all => all)
-                    }else{
-                        return resolved
-                    }
-                }).then(result => {
-                    fs.writeFileSync(filePath, JSON.stringify(result))
-                    res.send(result)
-                }).catch(err => {
-                    //console.log(err)
-                })
-            }
-        })
-    }
-    smhiAPI() {
-        smhi.init(this.app, 'corrected-archive');
-    }
-    smhi_stations() {
-        return smhi.stations.getStations();
     }
     createAPI(){
         this.webserver.http(this.app);
